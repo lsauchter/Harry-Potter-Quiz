49802ccea93f47b8b217e17bcc4855c4a5e77059
Lauren Auchter <lsauchter@gmail.com>
2019-04-15 11:30:52 -0400

    Created question template

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
+let questionNumber = 0;
+
+let score = 0;
+
 function startQuiz() {
     $('.startButton').click(function(event) {
         $('.startPage').remove();
+        renderQuestion();
     }
     )
+}
+
+function displayQuestionNumber() {
+    questionNumber++;
+}
+
+function questionTemplate() {
+    return `<div class="question">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form>
+    <fieldset class="answers">
+    <label>
+    <input type="radio" name="answer" required>
+    ${STORE[questionNumber].answer1}
+    </label><label>
+    <input type="radio" name="answer" required>
+    ${STORE[questionNumber].answer2}
+    </label>
+    <label>
+    <input type="radio" name="answer" required>
+    ${STORE[questionNumber].answer3}
+    </label>
+    <label>
+    <input type="radio" name="answer" required>
+    ${STORE[questionNumber].answer4}
+    </label>`
+}
+
+function renderQuestion() {
+    $('.questionForm').html(questionTemplate());
 }
 
 
 function runQuiz() {
     startQuiz();
-    handleSubmitButton();
-    nextQuestion();
-    handleRestartButton();
+   // handleSubmitButton();
+   // nextQuestion();
+  //  handleRestartButton();
 }
 
 $(runQuiz())
